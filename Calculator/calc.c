d44e992b92f076718b47e09009e6cf0ae8964905
Karl <karlcmoreno0811@gmail.com>
2023-04-04 21:48:38 +0000

    Learning how to use variables

--- Calculator/calc.c
+++ Calculator/calc.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 int main() {
-	printf("3 + 2 is %d\n", 3 +2);
-	printf("3 - 2 is %d\n", 3 - 2);
-	printf("3 * 2 is %d\n", 3*2);
-	printf("10 / 9 is %d\n", 10/9);
-	printf("3 %% 2 is %d\n", 3%2);
-	return 0;
+	int aNumber;
+	int oNumber;
+
+	aNumber = 5;
+	oNumber = 6;
+
+	printf("The sum is %d\n", aNumber + oNumber);
+	return (0);
 }
