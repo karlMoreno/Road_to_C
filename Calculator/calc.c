91f82f387234b08c94e794e81a7a18869988723e
Karl <karlcmoreno0811@gmail.com>
2023-04-04 21:42:01 +0000

    Learning how to print math

--- Calculator/calc.c
+++ Calculator/calc.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int main() {
-	
+	printf("3 + 2 is %d\n", 3 +2);
+	printf("3 - 2 is %d\n", 3 - 2);
+	printf("3 * 2 is %d\n", 3*2);
+	printf("10 / 9 is %d\n", 10/9);
+	printf("3 %% 2 is %d\n", 3%2);
 	return 0;
 }
