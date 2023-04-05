a0eda543b7f9b37a9a4914bc05801159ff02942e
Karl <karlcmoreno0811@gmail.com>
2023-04-05 17:38:00 +0000

    See what happens when we exceed the maximum number.

--- Calculator/calc.c
+++ Calculator/calc.c
@@ -2,9 +2,20 @@
 #include <stdint.h>
 
 int main() {
-	uint8_t smallNum = 255;
+	uint8_t smallNumber;
+	uint8_t result;
 
-	printf("255+1 is %u\n", smallNum+1);
+	smallNumber = 255;
+	result = smallNumber + 1;
+
+	printf("255+1 is %d\n", result);
 
 	return (0);
 }
+
+
+// Now the result is 0. Why? 
+// Because our calculation returned 
+// 0b1111 1111 + 0b000 0001 = 0b1 *0000 0000*. 
+// Only the starred portion was stored due to the limited 
+// space in which to store the variable.
