cb9a6efd8b7d01d3752d575a0840f4b169b5d7b0
Karl <karlcmoreno0811@gmail.com>
2023-04-05 17:28:11 +0000

    Simple use of uint8_t

--- Calculator/calc.c
+++ Calculator/calc.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
-int main() {
-	int8_t has8bits = 0x12;		// 8 bits
-	int16_t has16bits = 0x1234;
-	int32_t has32bits = 0x12345678; // 32 bits
-	int64_t has64bits = 0x123456789abcdef0; //64 bits
 
-	printf("8 bits %x\n", has8bits);
-	printf("16 bits %x\n", has16bits);
-	printf("32 bits %x\n", has32bits);
-	printf("64 bits %lx\n", has64bits);
-	 
-	return (0);
+int main() {
+	uint8_t smallNumber = 0x12; 
+
+	printf("0x12 is %u decimal\n", smallNumber);
+	printf("0x12 is %o octal\n", smallNumber);
+	printf("0x12 is %x hex\n", smallNumber);
+
 }
