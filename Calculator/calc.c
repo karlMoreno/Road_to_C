ec95cf01406d5894ab20b4f9dda6a1b8ccf3abbb
Karl <karlcmoreno0811@gmail.com>
2023-04-05 17:06:37 +0000

    Demonstrate different sizes of integers

--- Calculator/calc.c
+++ Calculator/calc.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <stdint.h>
 int main() {
-	short int aShortInt; // oh I can make comments like this thats cool
-	int aInteger;
-	long int aLongInt;
-	long long int aLongLongInt;
+	int8_t has8bits = 0x12;		// 8 bits
+	int16_t has16bits = 0x1234;
+	int32_t has32bits = 0x12345678; // 32 bits
+	int64_t has64bits = 0x123456789abcdef0; //64 bits
 
-	printf("Size of (short Int) = %ld (bytes) %ld bits\n", 
-	sizeof(aShortInt), sizeof(aShortInt)* 8);
-
-	printf("Size of a (int) = %ld (bytes) %ld bits\n",
-	 sizeof(aInteger), sizeof(aInteger)*8);
-
-	printf("Size of (long int) = %ld (bytes) %ld (bits)\n",
-	sizeof(aLongInt), sizeof(aLongInt)*8);
-
-	printf("Size of (long long int) = %ld (bytes) %ld (bits)\n",
-	 sizeof(aLongLongInt), sizeof(aLongLongInt)*8);
+	printf("8 bits %x\n", has8bits);
+	printf("16 bits %x\n", has16bits);
+	printf("32 bits %x\n", has32bits);
+	printf("64 bits %lx\n", has64bits);
 	 
 	return (0);
 }
