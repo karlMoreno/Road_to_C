3d1239e7e8b1c89c98d7de6f0dad31d3a51a72ec
Karl <karlcmoreno0811@gmail.com>
2023-04-08 02:00:55 +0000

    Hello World using string variable

--- Hangman/hangman.c
+++ Hangman/hangman.c
@@ -1,28 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
-    int numbers1[5] = {11, 12, 13, 14, 15}; // some numbers
-    int numbers2[5] = {21, 22, 23, 24, 25}; // variables to be overwritten
+const char hello[] = {'H','e','l','l','o', ' ',
+                            'W', 'o','r','l','d', '\0'}; // the characters to print
 
-    printf("numbers2[0] %d\n", numbers2[0]);
 
-    numbers1[6] = 99; //illegal
-
-    // illegal -- loops past end
-
-    for(int i = 0; i < 9; i++) {
-        printf("numbers1[%d] %p\n", i, &numbers1[i]);
-    }
-
-    printf("numbers2[%d] %p\n", 0, &numbers2[0]);
-    printf("numbers2[0] %d\n", numbers2[0]);
-
-    printf("numbers1[8] is %d\n", numbers1[8]);
-
-    printf("-----------------test----------\n");
-    printf("numbers1[%d] %p\n", 8, &numbers1[8]);
-    printf("numbers2[%d] %p\n", 0, &numbers2[0]);
+int main(){
+    puts(hello); // write string and new line
 
     return (0);
 }
