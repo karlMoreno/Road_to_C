1f4f8c49834639e65e6e099e2bff8163b8439f39
Karl <karlcmoreno0811@gmail.com>
2023-04-08 04:03:05 +0000

    /** * @brief Program * with pointers */

--- Hangman/hangman.c
+++ Hangman/hangman.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 
-const char hello[] = {'H','e','l','l','o', ' ',
-                            'W', 'o','r','l','d', '\0'}; // the characters to print
+char theData[5] = "1234"; // some data to play with
+
+char* allChange; // Pointer and value can change 
+
+const char* dataConst = "abc"; // char const, pointer not
+
+char* const ptrConst = theData; // char var, pointer not
+
+const char* const allConst = "abc"; // nobody change nothing
+
+int main() {
+    char otherData[5] = "abcd"; // some other data
+
+    allChange = otherData; // change pointer
+    *allChange = 'x'; //change data
+
+    dataConst = otherData; //change pointer
+    // *dataConst = 'x'; // illegal to change data
+
+    //ptrConst = otherData //illegal to change data
+    *ptrConst = 'x'; // change data 
+
+    //all const = otherData; //Illegal to change pointer
+    // *allConst = 'x';  //Illegal to change data
 
 
-int main(){
-    puts(hello); // write string and new line
+    
 
     return (0);
 }
+
