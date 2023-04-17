40a750cf98d29ecc42b554658ff7b37825927a3d
Karl <karlcmoreno0811@gmail.com>
2023-04-17 00:11:56 -0700

    stack frames

--- Baremetal/main.c
+++ Baremetal/main.c
@@ -1,28 +1,27 @@
-/**
-* @brief Program to demonstrate procedures and local
-variables */
-
 #include <stdio.h>
 
-// function that is called from another function
-void inner(void){
+// function called from another function
+void inner(void)
+{
     int i = 5;
     int k = 3;
 
     i = i + k;
-    printf("%d\n", i);
+    printf("%d", i);
 }
 
-//Outer level function
-void outer(void){
+// outer level function
+void outer(void)
+{
     int i = 6;
-    int j = 4;
-    i = j + i;
+    int j = 2;
+    int i = i + j;
+    printf("%d", i);
     inner();
-    printf("%d\n", i);
 }
 
-int main() {
+int main()
+{
     outer();
 
     return (0);
