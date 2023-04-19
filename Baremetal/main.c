f33497c7d9ec16aaa05c61640b1cbd360ee87409
Karl <karlcmoreno0811@gmail.com>
2023-04-19 11:35:19 -0700

    Enum Tricks

--- Baremetal/main.c
+++ Baremetal/main.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
 
-int factor (const int x)
-{
-    if (x == 1)
-    {
-        return (1);
-    }
-    return(x * factor(x-1));
-}
+// // WARNING: do not change this without changing colorNames
+// enum colorType {
+//     COLOR_RED,
+//     COLOR_BLUE,
+//     COLOR_GREEN
+// };
+// //Warning Do not changing this without changing colorTypes
+// static const char* const colorNames = {
+//     "COLOR_RED","COLOR_BLUE","COLOR_GREEN"
+// };
 
-int main ()
-{
-int result = factor(5);
-printf("5! is %d\n", result);
-    
-return (0);
+
+
+// This is the beginning of a clever trick to define both the values and
+// the names for the enum colorType. The list below will be used twice,
+// once to generate the value and once to generate names.
+
+
+#define COLOR_LIST                         \  // \ tells processor that the line is continued
+    DEFINE_ITEM(COLOR_RED),                     \
+    DEFINE_ITEM(COLOR_BLUE),                    \
+    DEFINE_ITEM(COLOR_GREEN)
+
+
+//define DEFINE_ITEM so it generates the actual values for the enum
+#define DEFINE_ITEM(X) X
+enum colorType{
+    COLOR_LIST
+};
+#undef DEFINE_ITEM
+
+//define DEFINE_ITEM so it generates the names of the enum
+#define DEFINE_ITEM(X) #X
+static const char* colorNames[] = {
+    COLOR_LIST
+};
+#undef DEFINE_ITEM
+
+
+int main (){
+
+
+    return (0);
 }
